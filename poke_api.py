bb578a4a6e4364deb2584d61facb51cbfd047448
uhirpara <122479361+uhirpara@users.noreply.github.com>
2023-03-14 00:26:29 -0400

    Update poke_api.py

--- poke_api.py
+++ poke_api.py
@@ -1,30 +1,25 @@
 import requests
 
+def get_info_poke(pokemon):
 
+    pokemon=str(pokemon)
+    url=f'https://pokeapi.co/api/v2/pokemon/{pokemon}'
 
-    
-    
+    resp_msg=requests.get(url)
 
-def get_info_poke(poke_name):
-
-    poke_name=str(poke_name).strip().lower()
-    Poke_url=f'https://pokeapi.co/api/v2/pokemon/{poke_name}'
-
-    resp_msg=requests.get(Poke_url)
-
-    print(f"Getting information for {poke_name}...",end='')
+    print(f"Getting information for {pokemon}...")
 
     if resp_msg.ok:
         print('success')
         return resp_msg
     else:
         print("failure")
-        print(f"Response code: {resp_msg.status_code} ({resp_msg.reason})")
+        print(f"Response code : 404)")
         
         
     return
 def main():
-    get_url=get_info_poke('6')
+    url=get_info_poke('6')
     return
 
 if __name__ == '_main_':
